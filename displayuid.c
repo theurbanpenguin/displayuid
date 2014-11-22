54086755a08e37d560676b540f354832db8bbfe2
theurbanpenguin <andrew.mallett@theurbanpenguin.com>
2014-11-22 13:18:13 +0000

    First edit

---
+++ displayuid.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+int main () {
+	int real = getuid();
+	int euid = geteuid();
+	printf("The real UID =: %d\n", real);
+	printf("The effective UID =: %d\n", euid);
+}
